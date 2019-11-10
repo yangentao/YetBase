7b4e3241745b64eab5dd02301d3436eb14a378b2
Entao <entaoyang@163.com>
2019-11-10 19:12:03 +0800

    1.1.6 log

--- YetBase/Classes/log.swift
+++ YetBase/Classes/log.swift
@@ -29,31 +29,48 @@ public extension LogLevel {
 }
 
 public struct LogItem: Hashable, Equatable, ToString {
-	public var level: LogLevel
-	public var tag: String = ""
-	public var message: String
-	public var time: TimeInterval = Date().timeIntervalSince1970
+	var level: LogLevel
+	var tag: String = ""
+	var message: String
+	var time: TimeInterval = Date().timeIntervalSince1970
 
 	public var toString: String {
 		Date(timeIntervalSince1970: time).format(fmt: .yyyy_MM_dd__HH_mm_ss_SSS) + " " + level.rawValue + ": " + tag + " " + message
 	}
 }
 
-public struct LogListenerHolder: Equatable {
+public typealias LogAcceptor = (LogItem) -> Bool
 
-	public weak var listener: LogListener?
+//强引用
+public struct LogPrinterItem: Equatable {
+	var printer: LogPrinter
+	var acceptor: LogAcceptor = { _ in
+		true
+	}
 
-	public static func ==(lhs: LogListenerHolder, rhs: LogListenerHolder) -> Bool {
+	public static func ==(lhs: LogPrinterItem, rhs: LogPrinterItem) -> Bool {
+		lhs.printer === rhs.printer
+	}
+}
+
+public struct LogListenerItem: Equatable {
+	weak var listener: LogListener? = nil
+	var acceptor: LogAcceptor = { _ in
+		true
+	}
+
+	public static func ==(lhs: LogListenerItem, rhs: LogListenerItem) -> Bool {
 		lhs.listener === rhs.listener
 	}
 }
 
 public class LogImpl {
 	private let lock: NSRecursiveLock = NSRecursiveLock()
-	private var listeners: [LogListenerHolder] = []
-	public var printer: LogPrinter = LogConsolePrinter()
+	private var printerItems: [LogPrinterItem] = []
+	private var listenerItems: [LogListenerItem] = []
 
 	public private(set ) var historyItems: [LogItem] = []
+
 	public var keepSize: Int = 1000 {
 		didSet {
 			if self.keepSize < 0 {
@@ -65,7 +82,21 @@ public class LogImpl {
 		}
 	}
 
-	public func filterItems(_ block: (LogItem) -> Bool) -> [LogItem] {
+	fileprivate init() {
+		set(printer: LogConsolePrinter())
+	}
+
+	deinit {
+		let ls = self.printerItems
+		ls.forEach { p in
+			p.printer.logFlush()
+			p.printer.logUninstall()
+		}
+	}
+}
+
+public extension LogImpl {
+	func filterItems(_ block: (LogItem) -> Bool) -> [LogItem] {
 		self.lock.lock()
 		defer {
 			self.lock.unlock()
@@ -73,53 +104,7 @@ public class LogImpl {
 		return self.historyItems.filter(block)
 	}
 
-	public func addListener(listener: LogListener) {
-		let a = LogListenerHolder(listener: listener)
-		self.listeners.addOnAbsence(a)
-	}
-
-	public func flush() {
-		self.printer.logFlush()
-	}
-
-	public func log(item: LogItem) {
-		lock.lock()
-		if self.keepSize > 0 {
-			if self.historyItems.count > self.keepSize + 10 {
-				let n = self.historyItems.count - self.keepSize
-				self.historyItems.removeFirst(n)
-			}
-			self.historyItems.append(item)
-		}
-		if printer.acceptLog(item: item) {
-			if item.level == .debug {
-				#if DEBUG
-					printer.logWrite(item: item)
-				#endif
-			} else {
-				printer.logWrite(item: item)
-			}
-		}
-		lock.unlock()
-		Task.fore {
-			var needClean = false
-			for l in self.listeners {
-				if let L = l.listener {
-					L.onLog(item: item)
-				} else {
-					needClean = true
-				}
-			}
-			if needClean {
-				self.listeners.removeAll {
-					$0.listener == nil
-				}
-			}
-		}
-
-	}
-
-	public func itemsToString(_ items: [Any?]) -> String {
+	func itemsToString(_ items: [Any?]) -> String {
 		var buf = ""
 		for a in items {
 			if let b = a {
@@ -130,6 +115,126 @@ public class LogImpl {
 		}
 		return buf
 	}
+}
+
+public extension LogImpl {
+	func add(listener: LogListener) {
+		self.listenerItems.addOnAbsence(LogListenerItem(listener: listener))
+	}
+
+	func add(listener: LogListener, acceptor: @escaping LogAcceptor) {
+		self.listenerItems.addOnAbsence(LogListenerItem(listener: listener, acceptor: acceptor))
+	}
+
+	func remove(listener: LogListener) {
+		self.listenerItems.removeAll { item in
+			item.listener === listener
+		}
+	}
+}
+
+public extension LogImpl {
+
+	func add(printer: LogPrinter) {
+		self.lock.lock()
+		defer {
+			self.lock.unlock()
+		}
+		self.printerItems.addOnAbsence(LogPrinterItem(printer: printer))
+	}
+
+	func add(printer: LogPrinter, acceptor: @escaping LogAcceptor) {
+		self.lock.lock()
+		defer {
+			self.lock.unlock()
+		}
+		self.printerItems.addOnAbsence(LogPrinterItem(printer: printer, acceptor: acceptor))
+	}
+
+	func set(printer: LogPrinter) {
+		self.clearPrinters()
+		self.add(printer: printer)
+	}
+
+	func set(printer: LogPrinter, acceptor: @escaping LogAcceptor) {
+		self.clearPrinters()
+		self.add(printer: printer, acceptor: acceptor)
+	}
+
+	func clearPrinters() {
+		self.lock.lock()
+		defer {
+			self.lock.unlock()
+		}
+		let ls = self.printerItems
+		self.printerItems = []
+		ls.forEach {
+			$0.printer.logUninstall()
+		}
+	}
+
+	func remove(printer: LogPrinter) {
+		self.lock.lock()
+		defer {
+			self.lock.unlock()
+		}
+		self.printerItems.removeAll { item in
+			item.printer === printer
+		}
+		printer.logUninstall()
+	}
+
+	func flush() {
+		self.lock.lock()
+		defer {
+			self.lock.unlock()
+		}
+		let ls = self.printerItems
+		ls.forEach {
+			$0.printer.logFlush()
+		}
+	}
+
+	func log(item: LogItem) {
+		self.lock.lock()
+		defer {
+			self.lock.unlock()
+		}
+		if self.keepSize > 0 {
+			if self.historyItems.count > self.keepSize + 20 {
+				let n = self.historyItems.count - self.keepSize
+				self.historyItems.removeFirst(n)
+			}
+			self.historyItems.append(item)
+		}
+		let ls = self.printerItems
+		for pi in ls {
+			if pi.acceptor(item) {
+				pi.printer.logWrite(item: item)
+			}
+		}
+
+		Task.fore {
+			var needClean = false
+			let ls = self.listenerItems
+			for l in ls {
+				if let L = l.listener {
+					if l.acceptor(item) {
+						L.onLog(item: item)
+					}
+				} else {
+					needClean = true
+				}
+			}
+			if needClean {
+				self.listenerItems.removeAll {
+					$0.listener == nil
+				}
+			}
+		}
+
+	}
+
 }
 
 public extension LogImpl {
@@ -218,15 +323,14 @@ public func loge(tag: String, _ ss: Any?...) {
 	Log.error(tag: tag, ss)
 }
 
-public protocol LogPrinter {
-	func acceptLog(item: LogItem) -> Bool
-	func logWrite(item: LogItem)
-	func logFlush()
+public protocol LogListener: class {
+	func onLog(item: LogItem)
 }
 
-public protocol LogListener: class {
-	func acceptLog(item: LogItem) -> Bool
-	func onLog(item: LogItem)
+public protocol LogPrinter: class {
+	func logWrite(item: LogItem)
+	func logFlush()
+	func logUninstall()
 }
 
 public class LogConsolePrinter: LogPrinter {
@@ -238,33 +342,7 @@ public class LogConsolePrinter: LogPrinter {
 
 	}
 
-	public func acceptLog(item: LogItem) -> Bool {
-		true
-	}
-}
+	public func logUninstall() {
 
-public class LogTreePrinter: LogPrinter {
-	public var printers: [LogPrinter] = []
-
-	public init(printers: [LogPrinter]) {
-		self.printers = printers
-	}
-
-	public func logWrite(item: LogItem) {
-		for p in printers {
-			if p.acceptLog(item: item) {
-				p.logWrite(item: item)
-			}
-		}
-	}
-
-	public func logFlush() {
-		for p in printers {
-			p.logFlush()
-		}
-	}
-
-	public func acceptLog(item: LogItem) -> Bool {
-		true
 	}
 }
